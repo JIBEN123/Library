a28b84beb4d15db3c208946f5227dbbda8fa2fe4
DaWangXXX <hxiang1118@163.com>
2015-09-12 14:24:12 +0800

    初始化项目

---
+++ Library/Library/Classes/Main/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Library
+//
+//  Created by XXX on 15/9/12.
+//  Copyright (c) 2015年 huangx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
